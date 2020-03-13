be320d6d174f74844f453dd87ced3715f3e889a4
Cat Gomez <catherinedgomez@gmail.com>
2020-03-13 05:23:46 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,6 +20,7 @@ def bonus
    }
   }
   epic_tragedy[:montague][:patriarch][:name] = "Michael Jordan"
+  epic_tragedy[:montague][:patriarch][:name] = "Michael Jordan"
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
 end
