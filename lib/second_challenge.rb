a34fe386628ed5b99b948f47ff534ff17f3b265c
Cat Gomez <catherinedgomez@gmail.com>
2020-03-13 04:56:17 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -12,12 +12,12 @@ def second_challenge
 },
     :capulet => {
       :patriarch => {
-      }
+      },
       :matriarch => {
-      }
+      },
       :heroine => {
-      }
+      },
       :heroine_friends => [
-      ]
+      ],
       }
 end
