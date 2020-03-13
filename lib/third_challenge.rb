fb6c89c21fbc85c2ca14b5a6a12f045a4217b1c0
Cat Gomez <catherinedgomez@gmail.com>
2020-03-13 05:03:54 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -6,11 +6,13 @@ def third_challenge
         :age => "53"
         },
       :matriarch => {
-        :name => "Lady Montague",
-        :age => "54"
+        :name => "Romeo",
+        :age => "15",
+        :status => "alive"
         },
       :hero => {
-        #your key/value pairs here
+        :name => "Lady Montague",
+        :age => "54"
         },
       :hero_friends => []
    },
